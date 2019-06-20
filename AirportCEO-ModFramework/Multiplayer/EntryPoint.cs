ff56208fe858bb018d90704bc7b32efebc53b7e6
Guus Dohmen <guusdohmen@hotmail.nl>
2019-06-20 21:18:15 +0200

    Created the mod project

---
+++ AirportCEO-ModFramework/Multiplayer/EntryPoint.cs
@@ -0,0 +1,43 @@
+﻿using ACMF.ModLoader;
+using ACMF.ModLoader.Attributes;
+using Harmony;
+using System.Reflection;
+using UnityEngine;
+
+namespace SampleModBasicMod
+{
+    [ACMFMod(id: "Dohmen.Guus.ACEOMP", name: "AirportCEO Multiplayer", modVersion: "0.1.0", requiredACMLVersion: "0.1.0")]   
+    public class EntryPoint
+    {
+        public static Config Config { get; private set; } = null;
+
+        [ACMFModEntryPoint]
+        public static void Entry(Mod mod)
+        {
+            System.Console.WriteLine($"[{mod.ModInfo.ID}] {mod.ModInfo.Name} executing.");
+            Config = ACMF.ModHelper.Config.ACMFConfigManager.LoadConfig<Config>(mod);
+            HarmonyInstance harmony = HarmonyInstance.Create(mod.ModInfo.ID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(GameController))]
+    [HarmonyPatch("Update")]
+    public class GameControllerPatcher
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            if (EntryPoint.Config.ALLOW_GAME_CONTROLLER_PATCH && Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.B))
+            {
+                ACMF.ModHelper.DialogPopup.DialogManager.QueueMessagePanel("Working Postfix Onto GameController::Update");
+                ACMF.ModHelper.Utilities.Logger.ShowNotification("Working Postfix Onto GameController::Update");
+            }
+        }
+    }
+
+    public class Config : ACMF.ModHelper.Config.ACMFConfig
+    {
+        public bool ALLOW_GAME_CONTROLLER_PATCH = true;
+    }
+}
